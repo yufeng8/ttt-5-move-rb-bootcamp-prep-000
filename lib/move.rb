81b1596bb74d4f32e73eafbfcc9b4452caa361ad
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-16 20:01:43 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,11 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(user_input)
+  if (user_input = "invalide")
+    return -1
+  else
+  input = user_input.to_i
+  input -= 1
+ end
+end
